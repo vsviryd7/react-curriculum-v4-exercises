eda6a49d283a29ec72595cbb2a6d8ccd25e4942f
vsviryd7 <ol.sviridenko777@gmail.com>
2026-04-05 23:53:14 -0400

    Lesson 01 - About Me component

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,21 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = "Olga";
+  const age = 39;
+  const hobbies = ["Traveling","Painting","Coding"];
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>   Hi! My name is {name}. I am {age} years old and I am learning React with Code the Dream. 
+        I enjoy growing my skills in programming and creating new projects.</p>
+       <h2>My Hobbies</h2>
+          <ul>
+            {hobbies.map((hobby, index) => (
+              <li key={index}>{hobby}</li>
+            ))}
+          </ul>
     </div>
   );
 }
