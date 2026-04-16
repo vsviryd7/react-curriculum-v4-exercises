657610545660d4354a02cdbdc7271ffdbff27193
vsviryd7 <ol.sviridenko777@gmail.com>
2026-04-16 16:34:50 -0400

    lessson 2 task

---
+++ src/exercises/lesson-02/SnackApp.jsx
@@ -0,0 +1,15 @@
+import SnackHeader from "./SnackHeader";
+import SnackList from "./SnackList";
+import SnackFooter from "./SnackFooter";
+
+function SnackApp() {
+  return (
+    <div>
+      <SnackHeader />
+      <SnackList />
+      <SnackFooter />
+    </div>
+  );
+}
+
+export default SnackApp;
---
+++ src/exercises/lesson-02/SnackFooter.jsx
@@ -0,0 +1,7 @@
+function SnackFooter(){
+	const year = new Date().getFullYear();
+
+	return <p> Olga's Snack Rankings © {year}</p>
+}
+
+export default SnackFooter;
